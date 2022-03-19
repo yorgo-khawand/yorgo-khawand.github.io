abde6bce3c123a9b471c8c0ad811af7bd36a43e5
Jean Khawand <22157081+jeankhawand@users.noreply.github.com>
2022-03-19 21:28:20 +0100

    website v2

---
+++ src/assets/items.js
@@ -0,0 +1,33 @@
+import blog from "../assets/blog.jpg";
+import github from "../assets/github.jpg";
+import linkedin from "../assets/linkedin.jpg";
+import matrix from "../assets/matrix.jpg";
+const items = [
+  {
+    title: "GitHub",
+    subtitle: "Check out ! my open source projects",
+    image: github,
+    link: "https://github.com/jeankhawand",
+  },
+
+  {
+    title: "LinkedIn",
+    subtitle: "Connect with me",
+    image: linkedin,
+    link: "https://www.linkedin.com/in/jeankhawand",
+  },
+  {
+    title: "Blog",
+    subtitle: "Read my latest posts",
+    image: blog,
+    link: "https://blog.jeankhawand.com",
+  },
+  {
+    title: "Matrix",
+    subtitle: "Reach me on Matrix network ",
+    image: matrix,
+    link: "https://matrix.to/#/@jean:tchncs.de",
+  },
+];
+
+export default items;
